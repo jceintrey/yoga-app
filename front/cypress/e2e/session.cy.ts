d7e67c6c2c5bb616418688fa0aba9facb4b74ba8
Jérémie Ceintrey <jeremie.ceintrey@gmail.com>
2025-03-03 16:42:50 +0100

    front-tests/e2e: Configure e2e for sessions  - test on session list page  - test on session detail page

--- front/cypress/e2e/session.cy.ts
+++ front/cypress/e2e/session.cy.ts
@@ -1,24 +1,27 @@
 /// <reference types="Cypress" />
 
 import {
-  jdoe,
   admin,
-  sessions,
   anothersamplesession,
+  jdoe,
+  sessions,
   teachers,
 } from './mockdata';
 
 describe('Session spec', () => {
+  beforeEach(() => {
+    const user = admin;
+    cy.login(user);
+  });
+  afterEach(() => {
+    cy.logout();
+  });
   it('should show session list', () => {
-    cy.login(admin);
     cy.url().should('include', '/sessions');
   });
 
   it('should an admin be able to create a session', () => {
-    const user = admin;
-    cy.login(user);
     cy.url().should('include', '/sessions');
-    //first get the teacher list
     cy.intercept('GET', '/api/teacher', {
       body: teachers,
     });
@@ -48,5 +51,118 @@ describe('Session spec', () => {
     cy.get('.mat-simple-snackbar')
       .should('exist')
       .and('contain', 'Session created !');
+
+    cy.url().should('include', '/sessions');
+  });
+
+  it('should an admin be able to click on detail button of a session', () => {
+    const sessionId = 2;
+    const session = sessions.find((s) => s.id === sessionId);
+    const teacher = session
+      ? teachers.find((t) => t.id === session.teacher_id)
+      : undefined;
+
+    if (!session || !teacher) {
+      throw new Error('Session or teacher not found');
+    }
+    cy.intercept('GET', `/api/session/${sessionId}`, {
+      body: session,
+    }).as('sessionIntercept');
+    cy.intercept('GET', `/api/teacher/${teacher.id}`, {
+      body: teacher,
+    }).as('teacherIntercept');
+
+    cy.contains('.item mat-card-title', session.name)
+      .parents('.item')
+      .find('mat-card-actions button')
+      .first()
+      .should('be.visible')
+      .click();
+
+    cy.wait('@sessionIntercept');
+    cy.wait('@teacherIntercept');
+
+    cy.url().should('include', `/sessions/detail/${sessionId}`);
+
+    cy.contains(session.name).should('be.visible');
+  });
+
+  it('should an admin be able to click on edit button of a session', () => {
+    const sessionId = 3;
+    const session = sessions.find((s) => s.id === sessionId);
+    const teacher = session
+      ? teachers.find((t) => t.id === session.teacher_id)
+      : undefined;
+
+    if (!session || !teacher) {
+      throw new Error('Session or teacher not found');
+    }
+    cy.intercept('GET', `/api/session/${sessionId}`, {
+      body: session,
+    }).as('sessionIntercept');
+    cy.intercept('GET', `/api/teacher`, {
+      body: teacher,
+    }).as('teacherIntercept');
+
+    cy.contains('.item mat-card-title', session.name)
+      .parents('.item')
+      .find('mat-card-actions button')
+      .eq(1)
+      .should('be.visible')
+      .click();
+
+    cy.wait('@sessionIntercept');
+    cy.wait('@teacherIntercept');
+
+    cy.url().should('include', `/sessions/update/${sessionId}`);
+    cy.get('input[formControlName=name]').should('have.value', session.name);
+  });
+
+  it('Should not be able to see and click on Create button if not an admin', () => {
+    cy.logout();
+    cy.login(jdoe);
+    cy.get('button[routerLink=create]').should('not.exist');
+  });
+
+  it.only('should a user be able to participate to a session if not already participating and if is not admin', () => {
+    cy.logout();
+    cy.login(jdoe);
+
+    if (jdoe.admin) throw new Error('User should not be admin for that test');
+
+    const sessionId = 3;
+    const session = sessions.find((s) => s.id === sessionId);
+    if (session) {
+      session.users = session.users.filter((userId) => userId !== jdoe.id);
+    }
+
+    const teacher = session
+      ? teachers.find((t) => t.id === session.teacher_id)
+      : undefined;
+
+    if (!session || !teacher) {
+      throw new Error('Session or teacher not found');
+    }
+    cy.intercept('GET', `/api/session/${sessionId}`, {
+      body: session,
+    }).as('sessionIntercept');
+    cy.intercept('GET', `/api/teacher/${teacher.id}`, {
+      body: teacher,
+    }).as('teacherIntercept');
+
+    cy.contains('.item mat-card-title', session.name)
+      .parents('.item')
+      .find('mat-card-actions button')
+      .eq(0)
+      .should('be.visible')
+      .click();
+
+    cy.intercept('POST', `/api/session/${sessionId}/participate/${jdoe.id}`, {
+      body: teacher,
+    }).as('participate');
+
+    cy.get('button[data-testid=participate-button]').should('exist').click();
+
+    cy.wait('@participate');
   });
 });
