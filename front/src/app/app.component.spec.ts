c1df258c9d71aa7063e51fc05b563e6ac41432c8
Jérémie Ceintrey <jeremie.ceintrey@gmail.com>
2025-03-03 09:34:49 +0100

    Unit-tests: Configure unit tests for AppComponent  - test loggedIn  - test logout

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,29 +1,55 @@
 import { HttpClientModule } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { SessionService } from './services/session.service';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let sessionService: SessionService;
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        HttpClientModule,
-        MatToolbarModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [HttpClientModule, MatToolbarModule, RouterTestingModule],
+      declarations: [AppComponent],
+      providers: [SessionService],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+
+    fixture.detectChanges();
+    sessionService = TestBed.inject(SessionService);
+    router = TestBed.inject(Router);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should return is logged as a boolean Observable', () => {
+    //Arrange
+    jest.spyOn(sessionService, '$isLogged').mockReturnValue(of(true));
+    //Act and check
+    app.$isLogged().subscribe((isLogged) => expect(isLogged).toBe(true));
+  });
+
+  it('should logout and navigate to root path', async () => {
+    //Arrange
+    const logoutSpy = jest.spyOn(sessionService, 'logOut');
+    const navigateSpy = jest.spyOn(router, 'navigate').mockResolvedValue(true);
+    //Act
+    await app.logout();
+    await fixture.whenStable();
+    //Check
+    expect(logoutSpy).toHaveBeenCalled();
+    expect(navigateSpy).toHaveBeenCalledWith(['']);
   });
 });
